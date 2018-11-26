e08fc1305966e67c2d4249b534769b611d24d94d
Rexman17 <a.stone1794@gmail.com>
2018-11-26 13:12:21 -0500

    Done.

--- index.js
+++ index.js
@@ -73,7 +73,3 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 }) // end of DOMContentLoaded
-
-// TODO:
-// Above your list of monsters, you should have a form to create a new monster. You should have fields for name, age, and description, and a 'Create Monster Button'. When you click the button, the monster should be added to the list and saved in the API.
-// At the end of the list of monsters, show a button. When clicked, the button should load the next 50 monsters and show them.
